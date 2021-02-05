0e4ed80f3ebc0990817906e0a091aea1cf834d4e
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-02-04 22:42:37 -0300

    Enviar dados relativos ao pagamento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,6 +2,8 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => 
         res.send('Você está na roda de atendimento e está realizando um GET'));
 
-    app.post('/atendimentos',(req, res) => 
-        res.send('Você está na rota de atendimento e está realizando um POST'));
+    app.post('/atendimentos',(req, res) =>  {
+        console.log(req.body);
+        res.send('Você está na rota de atendimento e está realizando um POST')
+    });
 }
