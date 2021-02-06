184abe9e243bc5aed040934670c5ebefc9646177
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-02-06 20:21:36 -0300

    Criando as Tabelas

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpress');
 const connection = require('./infra/connection');
+const Tables = require('./infra/tables');
 
 connection.connect(err => {
     if (err)
         console.log(err);
     else {
         console.log('conectado com sucesso!');
-        
+       
+        Tables.init(connection);
         const app = customExpress();
         app.listen(3000,() => console.log('servidor rodando na porta 3000'));
     }
